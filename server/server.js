2ad689c8bdfe4b370cd62ba8cce046e0e5a8ef39
mrsimonslice <mrsimonslice@gmail.com>
2019-06-28 13:33:28 +0700

    prepare to deploy

--- server/server.js
+++ server/server.js
@@ -26,12 +26,12 @@ app.use(
     })
 );
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 
 function main() {
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
