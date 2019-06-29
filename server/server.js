47ead91313f88c00b82dcf03a7eb879e8bbf28ae
mrsimonslice <mrsimonslice@gmail.com>
2019-06-29 13:31:57 +0700

    edit env

--- server/server.js
+++ server/server.js
@@ -26,13 +26,12 @@ app.use(
     })
 );
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client", "build")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
-}
 
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+}
 
 
 function main() {
