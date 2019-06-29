c2a168192727d1b5b02669edbedf92f63dea55ad
mrsimonslice <mrsimonslice@gmail.com>
2019-06-29 13:32:19 +0700

    edit env

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-}
+
 
 
 function main() {
